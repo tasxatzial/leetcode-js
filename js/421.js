6cd13ddfc37fb6319d67ccb9c75655f34cb77b13
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-01-29 01:09:36 +0200

    421: fix identation

--- js/421.js
+++ js/421.js
@@ -10,7 +10,7 @@ const maxPow2 = 2 ** 31;
  */
 let toBinary = function(num) {
   let bin = [];
-  for (let i = 31; i>=0; i--){
+  for (let i = 31; i>=0; i--) {
     bin.push((num >> i) & 1)
   }
   return bin;
@@ -40,50 +40,48 @@ let toDecimal = function(bin) {
  * @param {number} bit 
  * @returns {Object}
  */
- let createNode = function(bit) {
-    return {val: bit,
-           left: undefined,
-           right: undefined};
-  }
+let createNode = function(bit) {
+  return {val: bit, left: undefined, right: undefined};
+}
   
-  /**
-   * Adds a new node as a child of the given parentNode. The node value is the given bit.
-   * Does not add any node if the given bit already exists as a value of either children.
-   * Returns the child node.
-   * 
-   * bit must take values 0 or 1.
-   * 
-   * @param {Object} parentNode 
-   * @param {number} bit 
-   * @returns {Object}
-   */
-  let addNode = function(parentNode, bit) {
-    if (bit === 1) {
-      if (parentNode.right === undefined) {
-        parentNode.right = createNode(bit);
-      }
-      return parentNode.right;
-    } else {
-      if (parentNode.left === undefined) {
-        parentNode.left = createNode(bit);
-      }
-      return parentNode.left;
+/**
+ * Adds a new node as a child of the given parentNode. The node value is the given bit.
+ * Does not add any node if the given bit already exists as a value of either children.
+ * Returns the child node.
+ * 
+ * bit must take values 0 or 1.
+ * 
+ * @param {Object} parentNode 
+ * @param {number} bit 
+ * @returns {Object}
+ */
+let addNode = function(parentNode, bit) {
+  if (bit === 1) {
+    if (parentNode.right === undefined) {
+      parentNode.right = createNode(bit);
     }
-  }
-  
-  /**
-   * Adds num (decimal) to the given tree.
-   * 
-   * @param {Object} treeRoot 
-   * @param {number} num 
-   */
-  let treeAdd = function(treeRoot, num) {
-    let bin = toBinary(num);
-    let curr = treeRoot;
-    for (let i = 0; i < bin.length; i++) {
-      curr = addNode(curr, bin[i]);
+    return parentNode.right;
+  } else {
+    if (parentNode.left === undefined) {
+      parentNode.left = createNode(bit);
     }
+    return parentNode.left;
   }
+}
+
+/**
+ * Adds num (decimal) to the given tree.
+ * 
+ * @param {Object} treeRoot 
+ * @param {number} num 
+ */
+let treeAdd = function(treeRoot, num) {
+  let bin = toBinary(num);
+  let curr = treeRoot;
+  for (let i = 0; i < bin.length; i++) {
+    curr = addNode(curr, bin[i]);
+  }
+}
 
   /**
  * Returns an array that represents the binary number X for which X ^ num is maximum.
@@ -93,30 +91,30 @@ let toDecimal = function(bin) {
  * @returns {number[]}
  */
 let findMaxXORBin = function(treeRoot, num) {
-    let bin = toBinary(num);
-    let res = [];
-    let curr = treeRoot;
-    for (let i = 0; i < bin.length; i++) {
-      if (bin[i] === 1) {
-        if (curr.left !== undefined) {
-          res.push(0);
-          curr = curr.left;
-        } else {
-          res.push(1);
-          curr = curr.right;
-        }
+  let bin = toBinary(num);
+  let res = [];
+  let curr = treeRoot;
+  for (let i = 0; i < bin.length; i++) {
+    if (bin[i] === 1) {
+      if (curr.left !== undefined) {
+        res.push(0);
+        curr = curr.left;
       } else {
-        if (curr.right !== undefined) {
-          res.push(1);
-          curr = curr.right;
-        } else {
-          res.push(0);
-          curr = curr.left;
-        }
+        res.push(1);
+        curr = curr.right;
+      }
+    } else {
+      if (curr.right !== undefined) {
+        res.push(1);
+        curr = curr.right;
+      } else {
+        res.push(0);
+        curr = curr.left;
       }
     }
-    return res;
   }
+  return res;
+}
 
   /**
  * Returns the maximum XOR between any two numbers in the given array.
@@ -125,20 +123,19 @@ let findMaxXORBin = function(treeRoot, num) {
  * @returns {number}
  */
 let findMaximumXOR = function(nums) {
-    let root = createNode(-1);
-    for (let i = 0; i < nums.length; i++) {
-     treeAdd(root, nums[i]);
-    }
-    let max = 0;
-    while (nums.length > 1) {
-      let lastNum = nums[nums.length - 1];
-      let res = findMaxXORBin(root, lastNum);
-      let maxXOR = toDecimal(res) ^ lastNum;
-      if (maxXOR > max) {
-        max = maxXOR;
-      }
-      nums.pop();
-    }
-    return max;
+  let root = createNode(-1);
+  for (let i = 0; i < nums.length; i++) {
+    treeAdd(root, nums[i]);
   }
-  
+  let max = 0;
+  while (nums.length > 1) {
+    let lastNum = nums[nums.length - 1];
+    let res = findMaxXORBin(root, lastNum);
+    let maxXOR = toDecimal(res) ^ lastNum;
+    if (maxXOR > max) {
+      max = maxXOR;
+    }
+    nums.pop();
+  }
+  return max;
+}
