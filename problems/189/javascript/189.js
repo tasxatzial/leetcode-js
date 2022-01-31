e07563ecd2e9d0e908947f992a6338a7422dfd99
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-01-31 04:20:24 +0200

    189 solution 1: array splice & slice

---
+++ problems/189/javascript/189.js
@@ -0,0 +1,17 @@
+/* Approach 1: Using splice & slice
+
+Time complexity: O(N)
+Space complexity: O(N)
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    let i = nums.length - k % nums.length;
+    let firstNums = nums.slice(0, i);
+    nums.splice(0, i);
+    nums.push(...firstNums);
+};
