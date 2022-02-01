4b13de7d210907871165eea81985d64c33c16721
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-02-01 04:04:00 +0200

    121: find max profit

---
+++ problems/121/javascript/121.js
@@ -0,0 +1,24 @@
+/* Approach: Linear pass 
+
+Time complexity: O(N)
+Space complexity: O(1)
+*/
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+ var maxProfit = function(prices) {
+    let minIdx = 0;
+    let maxProfit = 0;
+    for (let i = 1; i < prices.length; i++) {
+        let profit = prices[i] - prices[minIdx];
+        if (profit > maxProfit) {
+            maxProfit = profit;
+        }
+        else if (prices[i] < prices[minIdx]) {
+            minIdx = i;
+        }
+    }
+    return maxProfit;
+};
