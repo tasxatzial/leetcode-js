ee2380de6b53c940e8cf8622c2379622d36f16c0
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-02-09 11:54:17 +0200

    532 solution 2: refactor to use a hash map/set depending on k

--- problems/532/javascript/532-2.js
+++ problems/532/javascript/532-2.js
@@ -1,4 +1,4 @@
-/* Approach: Hash map
+/* Approach: Hash map / Hash set
 
 N = array length
 
@@ -14,24 +14,59 @@ Space complexity: O(N)
  * @return {number}
  */
  var findPairs = function(nums, k) {
+    if (k === 0) {
+        return findPairs0diff(nums); // hash map
+    } else {
+        return findPairskdiff(nums, k); // hash set
+    }
+};
+
+/**
+ * Given an array of integers nums, returns the number of unique 0-diff pairs in the array.
+ *
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findPairs0diff = function(nums) {
     let map = new Map();
     let i = 0;
     let totalPairs = 0;
     while (i < nums.length) {
         let fr = map.get(nums[i]);
-        if (fr !== undefined) {
-            if (k === 0 && fr < 2) {
-               totalPairs++;
-               map.set(nums[i], 1 + fr) 
-            }
-        } else {
-            if (map.has(nums[i] + k)) {
+        if (fr === 1) {
+            totalPairs++;
+            map.set(nums[i], 2);
+        } else if (fr === undefined) {
+            map.set(nums[i], 1);
+        }
+        i++;
+    }
+    return totalPairs;
+};
+
+/**
+ * Given an array of integers nums and an integer k, returns the number of unique k-diff pairs in the array.
+ * 
+ * Assumes k != 0.
+ *
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findPairskdiff = function(nums, k) {
+    let set = new Set();
+    let i = 0;
+    let totalPairs = 0;
+    while (i < nums.length) {
+        if (!set.has(nums[i])) {
+            if (set.has(nums[i] + k)) {
                 totalPairs++;
             }
-            if (map.has(nums[i] - k)) {
+            if (set.has(nums[i] - k)) {
                 totalPairs++;
             }
-            map.set(nums[i], 1); 
+            set.add(nums[i]); 
         }
         i++;
     }
