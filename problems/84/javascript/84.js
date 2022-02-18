87460e403d9187f8f7ed50dcd4d8cf749619841a
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-02-18 13:48:42 +0200

    84: find the previous smaller element

---
+++ problems/84/javascript/84.js
@@ -0,0 +1,25 @@
+/**
+ * Returns an array where the element at index i is the index j of the closest number such that:
+ * heights[i] > heights[j] and i > j. If such number does not exist, the index is -1.
+ * 
+ * @param {number[]} heights 
+ * @returns 
+ */
+let previousSmallerElement = function(heights) {
+    let stack = [{index: -1, value: -1}];
+    let arr = [];
+    for (let i = 0; i < heights.length; i++) {
+        let last = stack[stack.length - 1];
+        if (heights[i] > last.value) {
+            arr.push(i - 1);
+        } else {
+            while (last.value >= heights[i]) {
+                stack.pop();
+                last = stack[stack.length - 1];
+            }
+            arr.push(last.index);
+        }
+        stack.push({index: i, value: heights[i]});
+    }
+    return arr;
+}
