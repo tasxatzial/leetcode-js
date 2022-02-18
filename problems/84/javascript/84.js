215048fa490d2bd9dcb0852671ec444096c18f59
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-02-18 13:50:53 +0200

    84: find the next smaller element

--- problems/84/javascript/84.js
+++ problems/84/javascript/84.js
@@ -23,3 +23,29 @@ let previousSmallerElement = function(heights) {
     }
     return arr;
 }
+
+/**
+ * Returns an array where the element at index i is the index j of the closest number such that:
+ * heights[i] > heights[j] and i < j. If such number does not exist, the index is heights.length.
+ * 
+ * @param {number[]} heights 
+ * @returns 
+ */
+let nextSmallerElement = function(heights) {
+    let stack = [{index: heights.length, value: -1}];
+    let arr = [];
+    for (let i = heights.length - 1; i >= 0; i--) {
+        let last = stack[stack.length - 1];
+        if (heights[i] > last.value) {
+            arr.push(i + 1);
+        } else {
+            while (last.value >= heights[i]) {
+                stack.pop();
+                last = stack[stack.length - 1];
+            }
+            arr.push(last.index);
+        }
+        stack.push({index: i, value: heights[i]});
+    }
+    return arr.reverse();
+}
