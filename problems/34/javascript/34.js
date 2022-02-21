3e3d36e136e2e14e2838cd8fb6df76122efbeb25
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-02-21 17:27:46 +0200

    34 solution 1: find first and last position

---
+++ problems/34/javascript/34.js
@@ -0,0 +1,76 @@
+/* Approach: Binary search
+
+N = array length
+
+Time complexity: O(logN)
+Space complexity: O(1)
+*/
+
+/**
+ * Searches the nums array for the given target. Returns an array of its first and last position or [-1, -1] if target is not found.
+ * 
+ * Assumes that nums is in increasing order.
+ * 
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    let first = binarySearchLowest(nums, target);
+    let last = binarySearchHighest(nums, target);
+    return [first, last];
+};
+
+/**
+ * Searches the nums array for the given target. Returns its first position or -1 if target is not found.
+ * 
+ * Assumes that nums is in increasing order.
+ * 
+ * @param {number[]} nums 
+ * @param {number} target 
+ * @returns 
+ */
+var binarySearchLowest = function(nums, target) {
+    let low = 0;
+    let high = nums.length - 1;
+    while (low < high) {
+        let mid = Math.floor((low + high) / 2);
+        if (nums[mid] >= target) {
+            high = mid;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+    if (nums[high] === target) {
+        return high;
+    }
+    return -1;
+};
+
+/**
+ * Searches the nums array for the given target. Returns its last position or -1 if target is not found.
+ * 
+ * Assumes that nums is in increasing order.
+ * 
+ * @param {number[]} nums 
+ * @param {number} target 
+ * @returns 
+ */
+var binarySearchHighest = function(nums, target) {
+    let low = 0;
+    let high = nums.length - 1;
+    while (low < high) {
+        let mid = Math.ceil((low + high) / 2);
+        if (nums[mid] <= target) {
+            low = mid;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    if (nums[low] === target) {
+        return low;
+    }
+    return -1;
+};
