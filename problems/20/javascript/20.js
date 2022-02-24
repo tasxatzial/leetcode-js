15c8805e561a3031b6eb06e49e3be2fac284085c
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-02-24 16:02:00 +0200

    20: solution 1 (stack)

---
+++ problems/20/javascript/20.js
@@ -0,0 +1,37 @@
+/* Approach: Stack
+
+N = string length
+
+Time complexity: O(N)
+Space complexity O(N)
+*/
+
+let matchingBracket = (function() {
+    let map = new Map();
+    map.set('(', ')');
+    map.set('[', ']');
+    map.set('{', '}');
+    return map;
+})();
+
+/**
+ * "Returns true if the given string has properly matched brackets, false otherwise.
+ * 
+ * @param {string} s
+ * @return {boolean}
+ */
+let isValid = function(s) {
+    let stack = [];
+    for (let i = 0; i < s.length; i++) {
+        let currChar = s.charAt(i);
+        if (matchingBracket.get(currChar) !== undefined) {
+            stack.push(currChar);
+        } else {
+            let top = stack.pop();
+            if (matchingBracket.get(top) !== currChar) {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+};
