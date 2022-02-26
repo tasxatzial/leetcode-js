868934158fb3730af1dab5535f15927a4788735c
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-02-26 17:05:40 +0200

    21: solution 1 (1 pass)

---
+++ problems/21/javascript/21.js
@@ -0,0 +1,56 @@
+/* Approach: 1 pass
+
+N = list1 length
+M = list2 length
+
+Time complexity: O(N + M)
+Space complexity: O(N + M)
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+/**
+ * Merge the two sorted lists in a one sorted list.
+ * 
+ * @param {*} list1 
+ * @param {*} list2 
+ * @returns 
+ */
+var mergeTwoLists = function(list1, list2) {
+    if (list1 === null) {
+        return list2;
+    }
+    if (list2 === null) {
+        return list1;
+    }
+    let mergedList = {};
+    let ptr1 = list1;
+    let ptr2 = list2;
+    let ptr3 = mergedList;
+    while (ptr1 !== null && ptr2 !== null) {
+        ptr3.next = {};
+        ptr3 = ptr3.next;        
+        if (ptr1.val <= ptr2.val) {
+            ptr3.val = ptr1.val;
+            ptr1 = ptr1.next;
+        } else {
+            ptr3.val = ptr2.val;
+            ptr2 = ptr2.next;
+        }
+    }
+    let remainingList = (ptr1 === null) ? ptr2 : ptr1;
+    while (remainingList !== null) {
+        ptr3.next = {};
+        ptr3 = ptr3.next;
+        ptr3.val = remainingList.val;
+        remainingList = remainingList.next;
+    }
+    ptr3.next = null;
+    return mergedList.next;
+};
