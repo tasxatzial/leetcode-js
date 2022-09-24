c77540e7a3b2e2232c4dbf9d5fd60610e1d9430e
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-09-25 02:57:29 +0300

    113: solution 1

---
+++ problems/113/javascript/113.js
@@ -0,0 +1,45 @@
+/* Approach: Tree traversal (recursive)
+
+N = number of elements in tree
+
+Time complexity: O(N)
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {number[][]}
+ */
+ var pathSum = function(root, targetSum) {
+    let result = [];
+    let pathToRoot = [];
+    findPaths(root, result, pathToRoot, targetSum);
+    return result;
+};
+
+var findPaths = function(node, result, pathToNode, target) {
+    if (node === null) {
+        return;
+    }
+    let newTarget =  target - node.val;
+    pathToNode.push(node.val);
+    if (node.left === null && node.right === null) {
+        if (newTarget === 0) {
+            result.push([...pathToNode]);
+        }
+    }
+    else {
+        findPaths(node.left, result, pathToNode, newTarget);
+        findPaths(node.right, result, pathToNode, newTarget);
+    }
+    pathToNode.pop();
+};
